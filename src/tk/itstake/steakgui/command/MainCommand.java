6442b8e498f478677ea1361822044570a28371f4
ITSTAKE <bexco2010@gmail.com>
2015-11-01 18:33:46 +0900

    License comments Added

--- src/tk/itstake/steakgui/command/MainCommand.java
+++ src/tk/itstake/steakgui/command/MainCommand.java
@@ -1,3 +1,30 @@
+/*
+ * MainCommand.java
+ *
+ * Copyright (c) 2015 ITSTAKE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ *
+ * it under the terms of the GNU General Public License as published by
+ *
+ * the Free Software Foundation, either version 3 of the License, or
+ *
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package tk.itstake.steakgui.command;
 
 import org.bukkit.Bukkit;
@@ -8,12 +35,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import tk.itstake.steakgui.SteakGUI;
-import tk.itstake.steakgui.menueditor.EditorMain;
 import tk.itstake.steakgui.gui.GUIItem;
 import tk.itstake.steakgui.gui.Menu;
 import tk.itstake.steakgui.itemtask.ItemTask;
+import tk.itstake.steakgui.menueditor.EditorMain;
 import tk.itstake.steakgui.util.MenuFileHandler;
-import tk.itstake.steakgui.variable.Variables;
 import tk.itstake.util.ConfigHandler;
 import tk.itstake.util.LanguageHandler;
 import tk.itstake.util.MessageHandler;
